956a679d02bf489050307c4eb1693918fdcf010a
Seer5Developer <selvamanithangaraj84@gmail.com>
2025-10-14 23:35:11 +0530

    Update ProjectTasks.jsx

--- src/pages/ProjectTasks.jsx
+++ src/pages/ProjectTasks.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { supabase } from "../utils/supabaseClient";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable } from "react-router-dom";
 
 export default function ProjectTasks() {
   const { projectId } = useParams();
